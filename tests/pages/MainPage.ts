d85936cf5f8e4097f86bc8dcadff9714649baba7
Ilya Nasennikov <nasennikov89@gmail.com>
2025-11-02 21:15:30 +0400

    project structure

---
+++ tests/pages/MainPage.ts
@@ -0,0 +1,8 @@
+import { Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class MainPage extends BasePage {
+  async open() {
+    await this.page.goto('https://rutube.ru/');
+  }
+}
