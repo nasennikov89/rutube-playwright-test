2ab7a350811974b8665f7c9dbd1091c582422b5c
Ilya Nasennikov <nasennikov89@gmail.com>
2025-11-04 00:30:48 +0400

    add main page fixtures

--- tests/specs/unautorizaded/mainPage.spec.ts
+++ tests/specs/unautorizaded/mainPage.spec.ts
@@ -1,4 +1,4 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
 test('Открытие главной страницы', async ({ page }) => {
@@ -6,22 +6,13 @@ test('Открытие главной страницы', async ({ page }) => {
   await mainPage.open();
 });
 
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCoockiesAlert();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
 
-test('Проверка доступности элементов табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCoockiesAlert();
+test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrecAriaSnapshot();
 });
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCoockiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuLocatorHasCorrecAriaSnapshot();
 });
